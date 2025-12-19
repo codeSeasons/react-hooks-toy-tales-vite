da83b3f90c5634acaedca93d1f58839d9254cb87
Collin Heath <cwhouar15@gmail.com>
2025-12-19 08:02:06 -0800

    display toy cards and fix img fetch error size

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,6 +6,17 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((r) => {
+        if (!r.ok) throw new Error("Failed to fetch toys");
+        return r.json();
+      })
+      .then((data) => setToys(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +29,8 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+
+      <ToyContainer toys={toys} />
     </>
   );
 }
